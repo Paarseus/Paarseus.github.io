ad0902243ed782e7580087c14aa475c88820898d
Paarseus <100014381+Paarseus@users.noreply.github.com>
2023-02-01 00:38:29 -0800

    update

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Set the date we're counting down to
-var countDownDate = new Date("Mar 5, 2023 15:37:25").getTime();
+var countDownDate = new Date("Feb 4 , 2023 23:59:59").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
